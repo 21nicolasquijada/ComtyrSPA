b1131a338b75da11a7e1635c3b8c19ba32554a19
21nicolasquijada <21nicolasquijada@gmail.com>
2025-08-20 17:47:04 -0400

    Sixth commit. Adding responsive design and trucks images.

--- ComtyrSPAPage/LandingPage/static/js/script.js
+++ ComtyrSPAPage/LandingPage/static/js/script.js
@@ -50,8 +50,6 @@ cambiarSlide();
 
 setInterval(cambiarSlide,9000);
 
-
-
 function ocultarTextoInfos() {
     const array_new_divs_info = [
         '<a href=""><i class="bi bi-whatsapp"></i><b>Whatsapp</b></a>',
@@ -91,111 +89,99 @@ function ocultarTextoInfos() {
 window.addEventListener("resize", ocultarTextoInfos);
 window.addEventListener("load", ocultarTextoInfos);
 
-const rectangulo = document.querySelector('.rectangulo-nosotros-1');
+var animation = { duration: 40000, easing: (t) => t }
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      
-      observer.unobserve(rectangulo);
+var slider = new KeenSlider("#my-keen-slider", {
+  loop: true,
+  renderMode: "performance",
+  drag: false,
+  created(s) {
+    s.moveToIdx(5, true, animation)
+  },
+  updated(s) {
+    s.moveToIdx(s.track.details.abs + 5, true, animation)
+  },
+  animationEnded(s) {
+    s.moveToIdx(s.track.details.abs + 5, true, animation)
+  },
+})
+
+var slider_2 = new KeenSlider("#my-keen-slider-2", {
+  loop: true,
+  renderMode: "performance",
+  drag: false,
+  created(s) {
+    s.moveToIdx(5, true, animation)
+  },
+  updated(s) {
+    s.moveToIdx(s.track.details.abs + 5, true, animation)
+  },
+  animationEnded(s) {
+    s.moveToIdx(s.track.details.abs + 5, true, animation)
+  },
+})
+
+const parrafoCamionPluma = document.getElementById("parrafo-camion-pluma");
+const parrafoGruaHorquilla = document.getElementById("parrafo-grua-horquilla");
+const camionPlumaTitulo = document.querySelector(".camion-pluma-titulo");
+const gruaHorquillaTitulo = document.querySelector(".grua-horquilla-titulo");
+
+function textoCarrusel(){
+  if(window.matchMedia("(max-width:862px)").matches){
+    if(camionPlumaTitulo.classList.contains("ocultar")){
+      camionPlumaTitulo.classList.remove("ocultar");
+      parrafoCamionPluma.classList.add("ocultar");
+      gruaHorquillaTitulo.classList.remove("ocultar");
+      parrafoGruaHorquilla.classList.add("ocultar");
     }
-  });
-});
-
-observer.observe(rectangulo);
-
-function navigation(slider) {
-  let wrapper, dots, arrowLeft, arrowRight
-
-  function markup(remove) {
-    wrapperMarkup(remove)
-    dotMarkup(remove)
-    arrowMarkup(remove)
-  }
-
-  function removeElement(elment) {
-    elment.parentNode.removeChild(elment)
-  }
-  function createDiv(className) {
-    var div = document.createElement("div")
-    var classNames = className.split(" ")
-    classNames.forEach((name) => div.classList.add(name))
-    return div
-  }
-
-  function arrowMarkup(remove) {
-    if (remove) {
-      removeElement(arrowLeft)
-      removeElement(arrowRight)
-      return
+  } else{
+    if(parrafoCamionPluma.classList.contains("ocultar")){
+      parrafoCamionPluma.classList.remove("ocultar");
+      camionPlumaTitulo.classList.add("ocultar");
+      parrafoGruaHorquilla.classList.remove("ocultar");
+      gruaHorquillaTitulo.classList.add("ocultar");
     }
-    arrowLeft = createDiv("arrow arrow--left")
-    arrowLeft.addEventListener("click", () => slider.prev())
-    arrowRight = createDiv("arrow arrow--right")
-    arrowRight.addEventListener("click", () => slider.next())
-
-    wrapper.appendChild(arrowLeft)
-    wrapper.appendChild(arrowRight)
   }
-
-  function wrapperMarkup(remove) {
-    if (remove) {
-      var parent = wrapper.parentNode
-      while (wrapper.firstChild)
-        parent.insertBefore(wrapper.firstChild, wrapper)
-      removeElement(wrapper)
-      return
-    }
-    wrapper = createDiv("navigation-wrapper")
-    slider.container.parentNode.appendChild(wrapper)
-    wrapper.appendChild(slider.container)
-  }
-
-  function dotMarkup(remove) {
-    if (remove) {
-      removeElement(dots)
-      return
-    }
-    dots = createDiv("dots")
-    slider.track.details.slides.forEach((_e, idx) => {
-      var dot = createDiv("dot")
-      dot.addEventListener("click", () => slider.moveToIdx(idx))
-      dots.appendChild(dot)
-    })
-    wrapper.appendChild(dots)
-  }
-
-  function updateClasses() {
-    var slide = slider.track.details.rel
-    slide === 0
-      ? arrowLeft.classList.add("arrow--disabled")
-      : arrowLeft.classList.remove("arrow--disabled")
-    slide === slider.track.details.slides.length - 1
-      ? arrowRight.classList.add("arrow--disabled")
-      : arrowRight.classList.remove("arrow--disabled")
-    Array.from(dots.children).forEach(function (dot, idx) {
-      idx === slide
-        ? dot.classList.add("dot--active")
-        : dot.classList.remove("dot--active")
-    })
-  }
-
-  slider.on("created", () => {
-    markup()
-    updateClasses()
-  })
-  slider.on("optionsChanged", () => {
-    console.log(2)
-    markup(true)
-    markup()
-    updateClasses()
-  })
-  slider.on("slideChanged", () => {
-    updateClasses()
-  })
-  slider.on("destroyed", () => {
-    markup(true)
-  })
 }
 
-var slider = new KeenSlider("#my-keen-slider", {}, [navigation])
+window.addEventListener("resize",textoCarrusel);
+window.addEventListener("load",textoCarrusel);
+
+
+const rectangulo = document.querySelector('.rectangulo-nosotros-1');
+const rectangulo_2 = document.querySelector('.rectangulo-nosotros-2')
+const row_nosotros_1 = document.querySelector('.row-1-nosotros');
+const row_nosotros_2 = document.querySelector('.row-2-nosotros');
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && entry.target === row_nosotros_1) {
+      rectangulo.classList.add("transicion-rectangulo-nosotros");
+    } else if(entry.isIntersecting && entry.target === row_nosotros_2){
+      rectangulo_2.classList.add("transicion-rectangulo-nosotros");
+    }
+  });
+}, {
+  threshold: 0.9 // porcentaje visible necesario (30%)
+});
+
+observer.observe(row_nosotros_1);
+observer.observe(row_nosotros_2);
+
+const parrafos_carrusel = document.querySelectorAll(".texto-carrusel > p");
+
+function cambiarContenidoParrafoCarrusel(){
+  if(window.matchMedia("(max-width:800px)").matches){
+    parrafos_carrusel.forEach((parrafo)=>{
+      parrafo.classList.add("ocultar");
+    });
+  } else{
+    parrafos_carrusel.forEach((parrafo)=>{
+      parrafo.classList.remove("ocultar");
+    });
+  }
+}
+
+window.addEventListener("resize", cambiarContenidoParrafoCarrusel);
+window.addEventListener("load", cambiarContenidoParrafoCarrusel);
