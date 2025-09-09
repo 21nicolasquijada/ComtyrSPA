78ce777fa2580c8b6bb4f740b1d770e06a16d3b1
21nicolasquijada <21nicolasquijada@gmail.com>
2025-09-08 21:17:38 -0300

    Adding trucks pages

--- ComtyrSPAPage/LandingPage/urls.py
+++ ComtyrSPAPage/LandingPage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('camion-pluma/', views.camion_pluma, name='camion-pluma'),
 ]
--- ComtyrSPAPage/LandingPage/views.py
+++ ComtyrSPAPage/LandingPage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def camion_pluma(request):
+    return render(request, 'camion-pluma.html')
